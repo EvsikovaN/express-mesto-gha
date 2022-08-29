c9213291234d0c0faf69db6b3500e405ccc1630e
EvsikovaN <evsikova32@gmail.com>
2022-08-29 17:39:55 +0300

    udp: controller createUser

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Tests](https://github.com/EvsikovaN/express-mesto-gha/actions/workflows/tests-13-sprint.yml/badge.svg)](https://github.com/EvsikovaN/express-mesto-gha/actions/workflows/tests-13-sprint.yml)
+[![Tests](https://github.com/EvsikovaN/express-mesto-gha/actions/workflows/tests-14-sprint.yml/badge.svg)](https://github.com/EvsikovaN/express-mesto-gha/actions/workflows/tests-14-sprint.yml)
 # Проект Mesto фронтенд + бэкенд
 
 ## Директории
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/userModels');
 const { errorMessage } = require('../utils/errorsMessage');
 
@@ -16,9 +17,23 @@ const getUserById = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
+    .then((user) => res.send(user))
     .catch((err) => errorMessage(err, req, res));
 };
 
